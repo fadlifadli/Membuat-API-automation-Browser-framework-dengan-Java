26dbef5a7471dfee498c7eb19b61b680d9d8935a
fadlifadli <fadli.azhari.elka@gmail.com>
2024-07-04 13:48:51 +0700

    OpenChrome&Firefox

--- src/test/java/selenium.java
+++ src/test/java/selenium.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 import java.time.Duration;
 
 
@@ -13,10 +18,10 @@ public class selenium {
     public void helloJayjay() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--window-size=1920,1080");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--window-size=1920,1080");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
@@ -24,4 +29,22 @@ public class selenium {
         Thread.sleep(5000);
         driver.quit();
     }
+
+    @Test
+    public void Firefoxjayjay() throws InterruptedException {
+        FirefoxOptions options = new FirefoxOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--window-size=1920,1080");
+//        driver = new ChromeDriver(options);
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get("https://jayjay.co/");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
 }
